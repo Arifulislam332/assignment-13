daa58a6d2e3bff69d7e41df05998024f727798f9
Arifulislam332 <ayatullaharif332@gmail.com>
2023-11-21 21:50:03 +0600

    arif

---
+++ app.js
@@ -0,0 +1,24 @@
+// ANS TO THE QUESTION NO:01
+// const morning = "feeding time";
+// const afternoon = "not feeding time";
+// if (afternoon < afternoon) {
+//   console.log("01. In the morning they eat no food");
+// } else {
+//   console.log("01. Morning food is served at afternoon");
+// }
+// ANS TO THE QUESTION NO:02 //
+// conditions
+const maximumWeight = "500 kg";
+const passengerWeight = "499 kg";
+if (passengerWeight <= maximumWeight) {
+  console.log("02. Passengers can easily the river easly");
+} else {
+  console.log("02. Passengers can not easily the river easly");
+}
+
+// ANS TO THE QUESTION NO:03 //
+const myName = "Ariful";
+const myHobby = "typing code";
+const sentence = `03. Hi, my name is ${myName}.My favorite hobby is ${myHobby}.`;
+
+console.log(sentence);
